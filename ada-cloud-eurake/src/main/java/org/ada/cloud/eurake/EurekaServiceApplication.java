bb82d87ff366a7c17b61fca45f129dc43a808966
ada1988 <1046562032@qq.com>
2017-06-08 11:21:00 +0800

    注册、发现服务中心

--- ada-cloud-eurake/src/main/java/org/ada/cloud/eurake/EurekaServiceApplication.java
+++ ada-cloud-eurake/src/main/java/org/ada/cloud/eurake/EurekaServiceApplication.java
@@ -3,7 +3,11 @@ package org.ada.cloud.eurake;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+/**
+ * @EnableEurekaServer 启用 服务注册、发现中心 的配置
+ * @author: CZD  
+ * @Createtime: 2017年6月8日
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServiceApplication {
