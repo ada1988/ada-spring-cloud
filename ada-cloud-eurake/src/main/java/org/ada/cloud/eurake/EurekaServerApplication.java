1ae892ca3008347138653a7bb093eee774e7e6ef
ada1988 <1046562032@qq.com>
2017-06-13 09:59:01 +0800

    eurake-provider\eurake-client

--- ada-cloud-eurake/src/main/java/org/ada/cloud/eurake/EurekaServerApplication.java
+++ ada-cloud-eurake/src/main/java/org/ada/cloud/eurake/EurekaServerApplication.java
@@ -5,14 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 /**
  * @EnableEurekaServer 启用 服务注册、发现中心 的配置
+ * 
+ * 通过@EnableEurekaServer注解启动一个服务注册中心提供给其他应用进行对话
  * @author: CZD  
  * @Createtime: 2017年6月8日
  */
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServiceApplication {
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServiceApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
--- ada-cloud-eureka-client/src/main/java/org/ada/cloud/client/EurekaClientApplication.java
+++ ada-cloud-eureka-client/src/main/java/org/ada/cloud/client/EurekaClientApplication.java
@@ -1,36 +1,24 @@
 package org.ada.cloud.client;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-
+/**
+ * @EnableDiscoveryClient 启用 服务发现 的配置
+ * @EnableFeignClients 启动Feign客户端
+ * @author: CZD  
+ * @Createtime: 2017年6月8日
+ */
 @EnableDiscoveryClient
+//@EnableEurekaClient //你的应用使用@EnableEurekaClient注解，那么只能使用eureka来发现服务实例
+@EnableFeignClients
 @SpringBootApplication
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
-    }
-}
-
-@RestController
-class ServiceInstanceRestController {
-
-    @Autowired
-    private DiscoveryClient discoveryClient;
-
-    @RequestMapping("/service-instances/{applicationName}")
-    public List<ServiceInstance> serviceInstancesByApplicationName(
-            @PathVariable String applicationName) {
-        return this.discoveryClient.getInstances(applicationName);
     }
 }
